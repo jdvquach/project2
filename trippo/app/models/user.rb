65c46a4a0d82659dd184e47accc710bdea5640b6
John Quach <John.DV.Quach@gmail.com>
2018-11-03 13:08:50 +1100

    Cloudinary setup

--- trippo/app/models/user.rb
+++ trippo/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  has_secure_password  #for password password_digest
+  has_secure_password   # makes 'password' field MANDATOR
+  validates :email, presence: true, uniqueness: true
+  has_many : reviews
+  before_save { self.email = email.downcase }
 
 end
