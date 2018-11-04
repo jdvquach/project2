641a2e1241a95398ff043d5562583e357dd79458
John Quach <John.DV.Quach@gmail.com>
2018-11-05 09:27:38 +1100

    user.rb and application.html.erb

--- trippo/app/models/user.rb
+++ trippo/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password   # makes 'password' field MANDATOR
   validates :email, presence: true, uniqueness: true
-  has_many : reviews
+  has_many :reviews
   before_save { self.email = email.downcase }
 
 end
