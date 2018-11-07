7e593401d33065a62a584cae12fccc92f9485e81
Brad Dong <braddong@hotmail.com>
2018-11-07 11:30:22 +1100

    deleted unused database for heroku

--- db/migrate/20181106033036_change_latitude_to_be_float_in_attractions.rb
+++
@@ -1,5 +0,0 @@
-class ChangeLatitudeToBeFloatInAttractions < ActiveRecord::Migration[5.2]
-  def change
-    change_column :attractions, :latitude, :float
-  end
-end
--- db/migrate/20181106033047_change_longitude_to_be_float_in_attractions.rb
+++
@@ -1,6 +0,0 @@
-class ChangeLongitudeToBeFloatInAttractions < ActiveRecord::Migration[5.2]
-  def change
-    change_column :attractions, :longitude, :float
-
-  end
-end
