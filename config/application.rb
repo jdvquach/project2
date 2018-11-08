435062d48bc7bbacbf832a7a7c281d0306c3d433
Brad Dong <braddong@hotmail.com>
2018-11-09 09:44:09 +1100

    deploy to heroku

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
 module Trippo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.initialize_on_precompile = false
+
     config.middleware.insert_before 0, Rack::Cors do
      allow do
        origins '*'
