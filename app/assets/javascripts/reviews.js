1edacdca0d9f0d5f5bcfa82d54c556fe9507abe1
leila-Haghdoost <haghdoost62@gmail.com>
2018-11-09 14:15:05 +1100

    attraction description field changes

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -12,7 +12,7 @@ $('#reviewSubmit').on('click',() =>{
   $.post(url, { comment, attraction_id })  //because variable and  key same
   .done( response => {
     // console.log(response);
-
+//this is to render the new comment above (prepend) the old reviews.  The older reviews are rendered by rails on the page load, but the new comment will be rendered by the code below instead of calling a get request.
     $(`<h6><strong>${response.user.name} | just now</strong></h6>
        <p>${response.comment}</p>
        <p><a href="/reviews/${response.id}" data-method="delete" rel="nofollow">Delete</a></p>
